a595239905da1a1ad6c0bae9b65a861047fc49dc
Brian Chambers <brianmchambers@proton.me>
2026-07-19 08:46:44 -0500

    Add initial ASP.NET Core Razor Pages project.

---
+++ razor1/razor1/Pages/Hello.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor1.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ razor1/razor1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor1.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
